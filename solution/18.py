ad2c46f76b450f5277983acba57bd5d731591d33
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-11-28 00:42:36 +0900

    Update 18.py

--- solution/18.py
+++ solution/18.py
@@ -1,28 +1,24 @@
-def count_sort(arr, k):
-  # ➊ 해시 테이블 생성 및 초기화
-  hashtable = [0] * (k + 1)
-
-  for num in arr:
-    # ➋ 현재 원소의 값이 k 이하인 때에만 처리
-    if num <= k:
-      # ➌ 현재 원소의 값을 인덱스로 해 해당 인덱스의 해시 테이블 값을 1로 설정
-      hashtable[num] = 1
-  return hashtable
-
 def solution(arr, target):
-  hashtable = count_sort(arr, target)
-
-  for num in arr:
-    complement = target - num
-    # ➍ target에서 현재 원소를 뺀 값이 해시 테이블에 있는지 확인
-    if (
-      complement != num
-      and complement >= 0
-      and complement <= target
-      and hashtable[complement] == 1
-    ):
-      return True
-  return False
+    hash = [0] *(target + 1) # ➊ 해시 테이블 생성 및 초기화
+    
+  # ➋ arr의 각 원소를 키로 해시테이블을 만듬
+    for num in arr:
+        if num <= target:
+            hash[num] = 1
+            
+    for num in arr:
+        # ➌ target보다 더 큰 수는 답이 될수 없음 
+        if(num >= target): 
+            continue
+       # ➍ arr에 중복되는 원소는 존재하지 않음 
+        if((target - num) == num):
+            continue
+        # ❺ 두 수의 합으로 target을 만들수 있으면 True반환 
+        if(hash[target - num]):
+            return True
+    
+    # ❻ 두 수의 합으로 target을 만들 수 없으면 False 반환
+    return False
   
 # TEST 코드 입니다. 주석을 풀고 실행시켜보세요
 # print(solution([1, 2, 3, 4, 8], 6)) # 반환값 : True
