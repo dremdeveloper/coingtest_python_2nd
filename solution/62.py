302cbf5a1ec2b5f63bd3315f9cdc21054fd0e5df
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-11-28 00:51:26 +0900

    Update 62.py

--- solution/62.py
+++ solution/62.py
@@ -1,28 +1,21 @@
-def solution(arr, n):
-  # ❶ 2차원 배열을 인자로 받고, 90도 회전시키는 함수
-  def rotate_90(arr):
-    # ❷ 배열의 크기를 저장
-    n = len(arr)
+# ❶ 2차원 배열을 90도 회전시키는 함수
+def rotate_90(arr):
+  n = len(arr)  
+  rotated_arr = [[0] * n for _ in range(n)]
 
-    # ❸ 배열의 크기와 동일한 2차원 배열 생성(초기값은 0)
-    rotated_arr = [[0] * n for _ in range(n)]
+  for i in range(n):
+    for j in range(n):
+      rotated_arr[j][n - i - 1] = arr[i][j]
 
-    # ❹ 배열을 90도회전
-    for i in range(n):
-      for j in range(n):
-        rotated_arr[j][n - i - 1] = arr[i][j]
-
-    # ❺ 90도로 회전한 배열 반환
-    return rotated_arr
-
-  # ❻ 원본배열 arr을 복사
+  return rotated_arr
+# ❷ 배열을 n번 90도 회전시키는 함수
+def solution(arr, rotations):
   rotated_arr = arr.copy()
-
-  # ❼ 90도 회전 함수 호출
-  for _ in range(n):
+  for _ in range(rotations):
     rotated_arr = rotate_90(rotated_arr)
 
   return rotated_arr
+
 
 # TEST 코드 입니다. 주석을 풀고 실행시켜보세요
 '''
