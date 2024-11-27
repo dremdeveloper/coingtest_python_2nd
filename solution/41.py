3738b1559df0e6184e3cdc2870d86a784dce30fa
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-11-28 00:47:43 +0900

    Update 41.py

--- solution/41.py
+++ solution/41.py
@@ -1,34 +1,27 @@
-def solution(graph, source):
-  # ➊ 그래프의 노드 수
-  num_vertices = len(graph)
+INF = 9999999999
 
-  # ➋ 거리 배열 초기화
-  distance = [float("inf")] * num_vertices
+def solution(num_vertices, edges, source):
+  # ❶ 간선 정보를 활용해서 인접 리스트를 생성
+  graph = [[] for _ in range(num_vertices)]
+  for edge in edges:
+    from_vertex, to_vertex, weight = edge
+    graph[from_vertex].append((to_vertex, weight))
+  
+  # ❷ 현재까지 구한 최소 비용을 INF로 설정(시작 노드는 제외)
+  distance = [INF] * num_vertices
   distance[source] = 0
-
-  # ➌ 직전 경로 배열 초기화
-  predecessor = [None] * num_vertices
-
-  # ➍ 간선 수 만큼 반복하여 최단 경로 갱신
-  for temp in range(num_vertices - 1):
+  
+  # ❸ 정점의 개수 -1 만큼 최소 비용을 갱신
+  for _ in range(num_vertices - 1):
     for u in range(num_vertices):
       for v, weight in graph[u]:
-        # ➎ 현재 노드 u를 거쳐서 노드 v로 가는 경로의 거리가 기존에 저장된 노드 v까지의 거리보다 짧은 경우
         if distance[u] + weight < distance[v]:
-          # ➏ 최단 거리를 갱신해줍니다.
           distance[v] = distance[u] + weight
-          # ➐ 직전 경로를 업데이트합니다.
-          predecessor[v] = u
-
-  # ➑ 음의 가중치 순회 체크
+  
+  # ❹ 음의 순환이 있는지 확인
   for u in range(num_vertices):
     for v, weight in graph[u]:
-      # ➒ 현재 노드 u를 거쳐서 노드 v로 가는 경로의 거리가 기존에 저장된 노드 v까지의 거리보다 짧은 경우
       if distance[u] + weight < distance[v]:
-        # ❿ 음의 가중치 순회가 발견되었으므로 [-1]을 반환합니다.
         return [-1]
-  return [distance, predecessor]
-
-# TEST 코드 입니다. 주석을 풀고 실행시켜보세요
-# print(solution([[(1, 4), (2, 3), (4, -6 )], [(3, 5)], [(1, 2)], [(0, 7), (2, 4)],[(2, 2)]],0)) #반환갑 : [[0, -2, -4, 3, -6], [None, 2, 4, 1, 0]]
-# print(solution([[(1, 5), (2, -1)], [(2, 2)], [(3, -2)], [(0, 2), (1, 6)]],0)) # 반환값 : [-1]
+  
+  return distance
